a20d127270205d9d38e5cd0eb7a44aa313bab0d1
GabGRZT <gabriel.grezit@gmail.com>
2023-02-15 11:48:41 +0100

    Save Classe interne marché

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -56,4 +56,59 @@ public class Village {
 		}
 		return chaine.toString();
 	}
+	
+	public static class Marche {
+		private Etal [] etals;
+		
+		public Marche (int nbEtal) {
+			this.etals = new Etal [nbEtal];
+			
+		}
+		private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+			etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+		private int trouverEtalLibre() {
+			for (int indiceEtals = 0; indiceEtals < etals.length; indiceEtals ++) {
+				if (etals[indiceEtals].isEtalOccupe() == false) {
+					return indiceEtals;
+				}
+			}
+			return -1;
+		}
+		private Etal [] trouverEtals (String produit) { 
+			int compteur = 0;
+			for (int indiceEtals = 0; indiceEtals < etals.length ;indiceEtals ++) {
+				if (etals[indiceEtals].isEtalOccupe()==true) {
+					compteur += 1;
+				}
+			}
+			Etal[] contProd = new Etal [compteur];	
+			for (int indiceEtal = 0; indiceEtal < etals.length ;indiceEtal ++) {
+				if (etals[indiceEtal].contientProduit(produit)){
+					contProd [indiceEtal] = etals[indiceEtal];
+				}
+			}
+			return contProd;
+		}
+		private Etal trouverVendeur (Gaulois gaulois) {
+			for (int indiceEtals = 0; indiceEtals < etals.length ;indiceEtals ++) {
+				if(etals[indiceEtals].getVendeur() == gaulois) {
+					return etals[indiceEtals];
+				}
+			}
+			return null;
+		}
+		private String afficherMache() {
+			int nbEtalVide = 0; 
+			for (int indiceEtals = 0; indiceEtals < etals.length ;indiceEtals ++) {
+				if(etals[indiceEtals].isEtalOccupe() == false) {
+					nbEtalVide += 1;
+				return etals[indiceEtals].afficherEtal();
+				}
+			}
+			return "Il reste" + nbEtalVide + " etals non utilises dans le marche.\n";
+		}
+		
+		
+		}
 }
